cc27979f21e7c3b9d7933517699f1fe96bc9b2d8
orira22 <oriroitzaid@gmail.com>
2023-04-11 01:31:24 +0300

    Finished the input of a, b and c, and their randomizing. Also checked the input when moving between activities.

--- app/src/main/java/com/example/ex072/MainActivity.java
+++ app/src/main/java/com/example/ex072/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.ex072;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random rnd;
+    EditText etA;
+    EditText etB;
+    EditText etC;
+    Intent si;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rnd = new Random();
+
+        etA = findViewById(R.id.etA);
+        etB = findViewById(R.id.etB);
+        etC = findViewById(R.id.etC);
+
+    }
+
+    public void rndNumbers(View view) {
+        etA.setText("" + rnd.nextInt(100));
+        etB.setText("" + rnd.nextInt(100));
+        etC.setText("" + rnd.nextInt(100));
+    }
+
+    public void solveEq(View view) {
+        if((etA.getText().toString().equals("")) || (etB.getText().toString().equals("")) ||
+                (etB.getText().toString().equals(""))){
+            Toast.makeText(this, "You can't leave an empty coefficient", Toast.LENGTH_LONG).show();
+        }
+        else{
+            si = new Intent(this, SolutionActivity.class);
+            si.putExtra("a", Double.parseDouble(etA.getText().toString()));
+            si.putExtra("b", Double.parseDouble(etB.getText().toString()));
+            si.putExtra("c", Double.parseDouble(etC.getText().toString()));
+            startActivityForResult(si, 1);
+        }
     }
 }
