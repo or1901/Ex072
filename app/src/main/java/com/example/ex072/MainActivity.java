aa412ff587160de1513a97d831eceb1d3b2c6160
orira22 <oriroitzaid@gmail.com>
2023-04-15 21:42:07 +0300

    Finished the app: -the design of the second activity is done -presented the solution of the equation in the second activity by loading a site -defined the moving back to the first activity with the solutions, and presented them there

--- app/src/main/java/com/example/ex072/MainActivity.java
+++ app/src/main/java/com/example/ex072/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.ex072;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.nio.IntBuffer;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
     EditText etA;
     EditText etB;
     EditText etC;
+    TextView solutionsTxt;
     Intent si;
+    int solutionsAmount;
+    double x1, x2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +35,21 @@ public class MainActivity extends AppCompatActivity {
         etB = findViewById(R.id.etB);
         etC = findViewById(R.id.etC);
 
+        solutionsTxt = findViewById(R.id.solutionsTxt);
     }
 
     public void rndNumbers(View view) {
-        etA.setText("" + rnd.nextInt(100));
+        etA.setText("" + (rnd.nextInt(99) + 1));
         etB.setText("" + rnd.nextInt(100));
         etC.setText("" + rnd.nextInt(100));
     }
 
     public void solveEq(View view) {
         if((etA.getText().toString().equals("")) || (etB.getText().toString().equals("")) ||
-                (etB.getText().toString().equals(""))){
-            Toast.makeText(this, "You can't leave an empty coefficient", Toast.LENGTH_LONG).show();
+                (etB.getText().toString().equals("")) || (etA.getText().toString().equals("-")) ||
+                (etB.getText().toString().equals("-")) || (etC.getText().toString().equals("-")) ||
+                (etA.getText().toString().equals("0"))){
+            Toast.makeText(this, "Illegal coefficient", Toast.LENGTH_LONG).show();
         }
         else{
             si = new Intent(this, SolutionActivity.class);
@@ -48,5 +58,29 @@ public class MainActivity extends AppCompatActivity {
             si.putExtra("c", Double.parseDouble(etC.getText().toString()));
             startActivityForResult(si, 1);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int source, int good, @Nullable Intent data_back) {
+        super.onActivityResult(source, good, data_back);
+
+        if (data_back != null) {
+            solutionsAmount = data_back.getIntExtra("solutions", 0);
+            x1 = data_back.getDoubleExtra("x1", 0);
+            x2 = data_back.getDoubleExtra("x2", 0);
+
+            switch (solutionsAmount){
+                case 0:
+                    solutionsTxt.setText("No real solutions were found");
+                    break;
+                case 1:
+                    solutionsTxt.setText("x = " + x1);
+                    break;
+                case 2:
+                    solutionsTxt.setText("x1 = " + x1 + "\n\nx2 = " + x2);
+                    break;
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/ex072/SolutionActivity.java
+++ app/src/main/java/com/example/ex072/SolutionActivity.java
@@ -2,13 +2,69 @@ package com.example.ex072;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class SolutionActivity extends AppCompatActivity {
+    WebView wV;
+    Intent gi;
+    double a, b, c;
+    double dis, x1, x2;
+    String BASE_URL = "https://www.mathpapa.com/quadratic-formula/?q=";
+    String expression;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solution);
+
+        wV = findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.setWebViewClient(new MyWebViewClient());
+
+        gi = getIntent();
+        a = gi.getDoubleExtra("a", 0);
+        b = gi.getDoubleExtra("b", 0);
+        c = gi.getDoubleExtra("c", 0);
+        expression = "" + a + "x^2+" + b + "x+" + c + "%3D0";
+
+        wV.loadUrl(BASE_URL + expression);
+    }
+
+    public void goBack(View view) {
+        x1 = 0;
+        x2 = 0;
+        dis = b * b - (4 * a * c);
+
+        if(dis < 0) {
+            gi.putExtra("solutions", 0);
+        }
+        else if(dis == 0){
+            gi.putExtra("solutions", 1);
+
+            x1 = (-b + Math.sqrt(dis)) / (2 * a);
+        }
+        else{
+            gi.putExtra("solutions", 2);
+
+            x1 = (-b + Math.sqrt(dis)) / (2 * a);
+            x2 = (-b - Math.sqrt(dis)) / (2 * a);
+        }
+
+        gi.putExtra("x1", x1);
+        gi.putExtra("x2", x2);
+        setResult(RESULT_OK, gi);
+        finish();
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
